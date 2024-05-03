fdc3fb6e166d3554ea5e28a27166391a8427eb1a
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2024-05-03 18:10:33 -0400

    commited out sps.json because it is not used

--- src/app/api/sp-json/route.js
+++ src/app/api/sp-json/route.js
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 import { join } from "path";
 
 export async function GET() {
-    const jsonFilePath = join(process.cwd(), "public", "JSON", "sps.json");
+    // const jsonFilePath = join(process.cwd(), "public", "JSON", "sps.json");
     const jsonContent = await readFile(jsonFilePath, "utf-8");
     const jsonData = JSON.parse(jsonContent);
 
